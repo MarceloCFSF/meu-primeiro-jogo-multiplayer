a06561a2da353310068b4554aa9cc18605de4118
Marcelo CFSF <marcelo.cfsf@gmail.com>
2020-02-06 15:44:53 +0000

    alterações

--- playground/meu-teste/public/game.js
+++ playground/meu-teste/public/game.js
@@ -8,10 +8,14 @@ export default function createGame(){
         }
     }
 
+    function setState(newState) {
+        Object.assign(state, newState)
+    }
+
     function addPlayer(command) {
         const playerId = command.playerId
-        const playerX = command.playerX
-        const playerY = command.playerY
+        const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random() * state.screen.width)
+        const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random() * state.screen.height)
 
         state.players[playerId] = {
             x: playerX,
@@ -103,6 +107,7 @@ export default function createGame(){
         movePlayer,
         addFruit,
         removeFruit,
-        state
+        state,
+        setState
     }
 }
--- playground/meu-teste/server.js
+++ playground/meu-teste/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
 import socketio from 'socket.io'
-import { Socket } from 'dgram'
 
 const app = express()
 const server = http.createServer(app)
@@ -11,21 +10,20 @@ const sockets = socketio(server)
 app.use(express.static('public'))
 
 const game = createGame()
-game.addPlayer({playerId: 'player1', playerX: 0, playerY: 0})
-game.addPlayer({playerId: 'player2', playerX: 7, playerY: 0})
-game.addPlayer({playerId: 'player3', playerX: 9, playerY: 0})
-game.addPlayer({playerId: 'player4', playerX: 5, playerY: 6})
-game.addFruit({fruitId: 'fruit1', fruitX: 3, fruitY: 3})
-game.addFruit({fruitId: 'fruit2', fruitX: 4, fruitY: 5})
-game.movePlayer({playerId: 'player1', keyPressed: 'ArrowRight'})
-
-console.log(game.state)
 
 sockets.on('connect', (socket) => {
     const playerId = socket.id
-    console.log(`> Player connect on Server with id: ${playerId}`)
+    console.log(`> Player connect: ${playerId}`)
+
+    game.addPlayer({ playerId: playerId})
+    //console.log(game.state);
 
     socket.emit('setup', game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({playerId: playerId})
+        console.log(`> Player disconnected: ${playerId} `)
+    })
 })
 
 server.listen(3000, () => {
